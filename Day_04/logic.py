1d554b5774c7b43797fcd91dd8ffb895ddd63019
Pawcio <pawlonazaruk@gmail.com>
2022-12-04 13:34:20 +0100

    Day_04 First task solution

--- Day_04/logic.py
+++ Day_04/logic.py
@@ -1,5 +1,4 @@
-from typing import List, Tuple
-from math import floor
+from typing import List
 
 INPUT_FILE = "input_file.txt"
 
@@ -16,5 +15,27 @@ def clean_data(data: List[str]) -> List[str]:
     return [x.replace("\n", "") for x in data]
 
 
+def parse_into_set(data: str) -> set:
+    start, end = (data.split("-"))
+    return set([i for i in range(int(start), int(end) + 1)])
+
+
 def task_solution() -> None:
-    pass
+    rows = clean_data(get_data_from_file(INPUT_FILE))
+
+    # First part
+    total: int = 0
+    for text in rows:
+        is_subset: int = 0
+        part_1, part_2 = text.split(",")
+        elf_1: set = parse_into_set(part_1)
+        elf_2: set = parse_into_set(part_2)
+        if elf_1.issubset(elf_2):
+            is_subset = 1
+        if elf_2 .issubset(elf_1):
+            is_subset = 1
+        total += is_subset
+    print(total)
+
+
+task_solution()
