8ff7eed615692f07c1d9828b25f265b65cc38a11
Pawcio <pawlonazaruk@gmail.com>
2022-12-05 21:43:53 +0100

    Day_05 First task solution

--- Day_05/logic.py
+++ Day_05/logic.py
@@ -1,6 +1,5 @@
 from typing import List
-from pprint import pprint
-
+import re
 
 INPUT_FILE = "input_file.txt"
 
@@ -14,23 +13,36 @@ def get_data_from_file(file: str) -> List[str]:
 
 
 def clean_data(data: List[str]) -> List[str]:
-    #return [x.replace("\n", "").replace("[", "").replace("]", "") for x in data]
     return [x.replace("\n", "") for x in data]
 
 
+def create_stacks(rows: List[str]) -> List[List[str]]:
+    tab: List[List[str]] = [[] for _ in range(9)]
+    for text in rows:
+        for i, letter in enumerate(text[1:-1:4]):
+            if letter not in " ":
+                tab[i].append(letter)
+    return tab
+
+
 def task_solution() -> None:
-    rows = clean_data(get_data_from_file(INPUT_FILE))
-    #pprint(rows)
-    cargo_crane = rows[9::-1]
-    #pprint(cargo_crane)
+    rows: List[str] = clean_data(get_data_from_file(INPUT_FILE))
 
-    tab = []
-    for row in rows[0::-1]:
-        pprint(f"{row}")
-        temp = []
-        for chars in row[::3]:
-            pprint(f"{chars=}")
+    # First part
+    cargo_order: List[str] = rows[7::-1]
+    cargo_movements: List[str] = rows[10:]
 
+    stacks: List[List[str]] = create_stacks(cargo_order)
+
+    for movement in cargo_movements:
+        quantity, from_stack, to_stack = [int(x) for x in re.findall(r"[0-9]+", movement)]
+        from_stack -= 1
+        to_stack -= 1
+        for i in range(0, quantity):
+            stacks[to_stack].append(stacks[from_stack].pop())
+
+    result: str = "".join([x[-1] for x in stacks])
+    print(f"{result=}")
 
 
 task_solution()
