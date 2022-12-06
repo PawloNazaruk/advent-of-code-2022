dceb46a2c6468debb0d42d71fea39f9b8679be1e
Pawcio <pawlonazaruk@gmail.com>
2022-12-06 21:16:24 +0100

    Day_06 Added

---
+++ Day_06/main.py
@@ -0,0 +1,5 @@
+from logic import task_solution
+
+
+if __name__ == "__name__":
+    task_solution()
---
+++ Day_06/task_solution.py
@@ -0,0 +1,22 @@
+from typing import List
+import re
+from pprint import pprint
+
+
+INPUT_FILE = "input_file.txt"
+
+
+def get_data_from_file(file: str) -> List[str]:
+    try:
+        with open(file, 'r+') as file_txt:
+            return file_txt.readlines()
+    except FileNotFoundError as err:
+        print(f"File({file}) not found due to: {err}")
+
+
+def clean_data(data: List[str]) -> List[str]:
+    return [x.replace("\n", "") for x in data]
+
+
+
+task_solution()
